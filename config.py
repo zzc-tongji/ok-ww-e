5a6bd6a3de805b7b44b44a442de962663a732e09
Zhicheng Zhang <20331014+zzc-tongji@users.noreply.github.com>
2026-01-02 14:09:54 +0000

    优化点击F (ok-oldking) 优化日常一条龙 (RU) 优化战斗点击F (ok-oldking) update document (Zhicheng Zhang) forgery task update from 10 to 15 (Zhicheng Zhang)

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import numpy as np
 from ok import ConfigOption
 from src.task.process_feature import process_feature
 
-version = "v3.0.6.0"
+version = "v3.0.8.0"
 
 
 def calculate_pc_exe_path(running_path):
