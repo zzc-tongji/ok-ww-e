0ea13fc01f059504fa9bbdfb5fb77bfd28968385
Zhicheng Zhang <20331014+zzc-tongji@users.noreply.github.com>
2026-01-12 01:44:59 +0000

    fix bug in domain task (Zhicheng Zhang) update build workflow (Zhicheng Zhang) Revert 'disable build from upstream' (Zhicheng Zhang)

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import numpy as np
 from ok import ConfigOption
 from src.task.process_feature import process_feature
 
-version = "v3.0.12.0"
+version = "v3.0.12.1"
 
 
 def calculate_pc_exe_path(running_path):
