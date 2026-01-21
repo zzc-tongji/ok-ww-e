56d5a0dc1601edc418ca07f37f46a35350c43b12
Zhicheng Zhang <20331014+zzc-tongji@users.noreply.github.com>
2026-01-21 06:23:50 +0000

    fix bug in auto restart for game update (Zhicheng Zhang)

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import numpy as np
 from ok import ConfigOption
 from src.task.process_feature import process_feature
 
-version = "v3.0.21.0"
+version = "v3.0.21.1"
 
 
 def calculate_pc_exe_path(running_path):
