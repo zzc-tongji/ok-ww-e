d3018fbd1fb27393cd23de9692709b396fc79eb2
Zhicheng Zhang <20331014+zzc-tongji@users.noreply.github.com>
2026-01-21 04:30:47 +0000

    增加游戏更新重启等待时间,修复QQ群链接 (ok-oldking) 修复战斗中停止任务无效 (ok-oldking) 优化战斗检测 (ok-oldking) 避免战斗点击F误触交互 (RU) 优化散华 (RU) 优化NightmareNestTask (RU) 优化赞妮 (RU) 使用check_boss_name (RU) 添加f_break释放依据，修改若干角色f的释放时机 (medicinemelancholy) 莫宁打完切人 (ok-oldking)

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import numpy as np
 from ok import ConfigOption
 from src.task.process_feature import process_feature
 
-version = "v3.0.17.2"
+version = "v3.0.21.0"
 
 
 def calculate_pc_exe_path(running_path):
