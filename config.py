1c5b55ee57ddf3b6305e517f4122aa38bfc98a39
Zhicheng Zhang <20331014+zzc-tongji@users.noreply.github.com>
2026-03-28 23:40:00 +0000

    日常一条龙支持运行后退出, 多账户支持添加定制任务 (ok-oldking) 拓宽了登录时账号检测框高度 (#1148) (QuanYong Zheng)

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ import numpy as np
 from ok import ConfigOption
 from src.task.process_feature import process_feature
 
-version = "v3.1.90.1"
+version = "v3.1.90.0"
 
 
 def calculate_pc_exe_path(running_path):
