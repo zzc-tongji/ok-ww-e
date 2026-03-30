0e6c6de3361b7e4e61292cb655f9c7ee23ee2b89
Zhicheng Zhang <20331014+zzc-tongji@users.noreply.github.com>
2026-03-30 03:02:45 +0000

    fix bug (Zhicheng Zhang) add sleep (Zhicheng Zhang) sync updates from DailyTask to DailyTask2 (Zhicheng Zhang) add retry to claim mail (Zhicheng Zhang) Revert fix bug of ui not ready (Zhicheng Zhang) fix bug of ui not ready (Zhicheng Zhang)

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ import numpy as np
 from ok import ConfigOption
 from src.task.process_feature import process_feature
 
-version = "v3.1.90.0"
+version = "v3.1.90.2"
 
 
 def calculate_pc_exe_path(running_path):
