49755a60ddd88772a6c4becd3ad19c9f4828f713
zzc-tongji <20331014+zzc-tongji@users.noreply.github.com>
2026-06-29 07:50:32 +0000

    patch (Zhicheng Zhang) update file readme/DailyTask.diff.html (github-actions[bot])

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ from qfluentwidgets import FluentIcon
 from ok import Box, ConfigOption
 from src.task.process_feature import process_feature
 
-version = "v3.4.15-rc.1"
+version = "v3.4.15-rc.2"
 
 
 def calculate_pc_exe_path(running_path):
